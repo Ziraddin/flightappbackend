02289548485aff3d821920812b88b144e96da9f8
Ziraddin Ismayilov <103113391+Ziraddin@users.noreply.github.com>
2023-12-03 14:07:27 +0400

    created connection with alwaysdata mariadb

--- src/main/kotlin/com/example/flightapp/FlightappApplication.kt
+++ src/main/kotlin/com/example/flightapp/FlightappApplication.kt
@@ -2,10 +2,18 @@ package com.example.flightapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FlightappApplication
 
 fun main(args: Array<String>) {
     runApplication<FlightappApplication>(*args)
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun hello() = "Hello World!"
 }
